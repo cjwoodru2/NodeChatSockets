c4109ef16478580d025ad35e5ceca346ffc5b920
Chris Woodruff <cjwoodru2@gmail.com>
2017-04-09 16:36:48 +0000

    workign web socket with socket.io

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
-
-const app = express();
+const socketIO = require("socket.io");
 
 // using join path to point statis to public dir
 const publicPath = path.join(__dirname + '/../public');
+const app = express();
+const server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
 
+io.on('connection', (socket) => {
+   console.log('new user connected'); 
+   
+   socket.on('disconnect', (socket) => {
+        console.log('user has disconnected')
+    })
+});
+
+
+
 // App listen to start server
-app.listen(process.env.PORT, process.env.IP, () => {
+server.listen(process.env.PORT, process.env.IP, () => {
     console.log('Chat Server is live!');
 });
 module.exports = { app };

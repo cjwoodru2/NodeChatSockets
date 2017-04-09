679a3dad73f45488e863275ce3004ee9d5852bc3
Chris Woodruff <cjwoodru2@gmail.com>
2017-04-09 16:10:08 +0000

    working web server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,15 @@
+const express = require("express");
 const path = require("path");
 
-const publicPath = path.join(__dirname + '/../public');
+const app = express();
 
-console.log(publicPath);
+// using join path to point statis to public dir
+const publicPath = path.join(__dirname + '/../public');
+app.use(express.static(publicPath));
+
+
+// App listen to start server
+app.listen(process.env.PORT, process.env.IP, () => {
+    console.log('Chat Server is live!');
+});
+module.exports = { app };

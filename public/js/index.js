bba2bd88fcf192d7706dd808c7d0dea9306e3542
Chris Woodruff <cjwoodru2@gmail.com>
2017-04-11 00:32:39 +0000

    server client basic messaging working - newMessage and createMessage socket events

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,17 @@
             
             socket.on('connect', function () {
                 console.log('connected to the server - socket!');
+                
+                socket.emit('createMessage', {
+                    from: 'JianYang',
+                    text: 'Yes'
+                });
             });
             
             socket.on('disconnect', function () {
                 console.log('disconnected from the server');
             });
+            
+            socket.on('newMessage', function (message) {
+                console.log('New message received', message)
+            })

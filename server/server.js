703a01f635ebb6c828d221f2ea77af9256b7f862
Chris Woodruff <cjwoodru2@gmail.com>
2017-04-13 13:56:03 +0000

    add broadcast new user joined and greeting all new users

--- server/server.js
+++ server/server.js
@@ -14,14 +14,33 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
    console.log('new user connected'); 
-
-   socket.on('createMessage', (message) => {
+    
+    // Welcome all joined users to the chat app
+        socket.emit('newMessage', {
+            from: 'Admin',
+            text: 'Welcome to Woody\'s Socket/Node powered chat app!',
+            createdAt: new Date().getTime()
+        });
+    // Alerts all users that a new user has joined
+        socket.broadcast.emit('newMessage', {
+            from: 'Admin',
+            text: 'A new user has joined',
+            createdAt: new Date().getTime()
+        });
+        
+    socket.on('createMessage', (message) => {
        console.log('createMessage', message);
-       io.emit('newMessage', {
-           from: message.from,
-           text: message.text,
-           createdAt: new Date().getTime()
-       });
+
+        io.emit('newMessage', {
+          from: message.from,
+          text: message.text,
+          createdAt: new Date().getTime()
+        });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
    });
    
    socket.on('disconnect', (socket) => {

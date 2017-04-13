ab746f3510076d4f17f8a49d7fcef4a9e75f6459
Chris Woodruff <cjwoodru2@gmail.com>
2017-04-13 15:06:46 +0000

    add generateMessage function to handle message creation

--- server/server.js
+++ server/server.js
@@ -4,10 +4,12 @@ const path = require("path");
 const socketIO = require("socket.io");
 
 // using join path to point statis to public dir
+const { generateMessage } = require("./utils/message");
 const publicPath = path.join(__dirname + '/../public');
 const app = express();
 const server = http.createServer(app);
 var io = socketIO(server);
+
 
 app.use(express.static(publicPath));
 
@@ -16,26 +18,15 @@ io.on('connection', (socket) => {
    console.log('new user connected'); 
     
     // Welcome all joined users to the chat app
-        socket.emit('newMessage', {
-            from: 'Admin',
-            text: 'Welcome to Woody\'s Socket/Node powered chat app!',
-            createdAt: new Date().getTime()
-        });
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Sockets/Node chat app'));
+    
     // Alerts all users that a new user has joined
-        socket.broadcast.emit('newMessage', {
-            from: 'Admin',
-            text: 'A new user has joined',
-            createdAt: new Date().getTime()
-        });
+        socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new user has joined'));
         
     socket.on('createMessage', (message) => {
        console.log('createMessage', message);
 
-        io.emit('newMessage', {
-          from: message.from,
-          text: message.text,
-          createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,

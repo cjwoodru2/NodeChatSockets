934bfd59d9c1a9b70a57458e977ff3f75971e2a1
Chris Woodruff <cjwoodru2@gmail.com>
2017-04-13 18:55:42 +0000

    working geo location link

--- public/js/index.js
+++ public/js/index.js
@@ -17,6 +17,15 @@
                 jQuery('#messages').append(li);
             });
             
+            socket.on('newLocationMessage', function (message) {
+                var li = jQuery('<li></li>');
+                var a = jQuery('<a target="_blank">My Current Location</a>')
+                
+                li.text(`${message.from}: `)
+                a.attr('href', message.url);
+                li.append(a);
+                jQuery('#messages').append(li);
+            })
             
             jQuery('#message-form').on('submit', function (e) {
                 e.preventDefault();
@@ -26,5 +35,21 @@
                     text: jQuery('[name=message]').val()
                 }, function () {
                     
-                })
-            })
+                });
+            });
+            
+            var locationButton = jQuery('#send-location');
+            locationButton.on('click', function () {
+               if (!navigator.geolocation) {
+                   return alert('Geolocation is not supported by your browser, dude.');
+               }
+               
+               navigator.geolocation.getCurrentPosition(function (position) {
+                   socket.emit('createLocationMessage', {
+                       latitude: position.coords.latitude,
+                       longitude: position.coords.longitude
+                   });
+               }, function () {
+                   alert ('unable to fetch location');
+               });
+            });

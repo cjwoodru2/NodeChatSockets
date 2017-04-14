da1846e9b34a75f28eeb4ef76c9025345eee72b5
Chris Woodruff <cjwoodru2@gmail.com>
2017-04-14 19:49:53 +0000

    add mothods for persisting users - getUser removeUser getUserList

--- server/utils/users.js
+++ server/utils/users.js
@@ -19,6 +19,27 @@ class Users {
         this.users.push(user);
         return user;
     }
+    
+    removeUser (id) {
+        /*return user that was removed*/
+        var user = this.getUser(id);
+        if (user) {
+            this.users = this.users.filter((user) => user.id !== id);
+        }
+        
+        return user;
+    }
+    
+    getUser (id) {
+        return this.users.filter((id) => user.id === id)[0];
+    }
+    
+    getUserList (room) {
+        var users = this.users.filter((user) => user.room === room);
+        var namesArray = users.map((user) => user.name);
+        
+        return namesArray;
+    }
 }
 
 module.exports = { Users };
